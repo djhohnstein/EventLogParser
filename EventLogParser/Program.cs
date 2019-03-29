f421751b1f43ddfafe9dd450e39a17234133bf7e
Dwight Hohnstein <dhohnstein@specterops.io>
2019-03-28 19:13:34 -0700

    first commit

---
+++ EventLogParser/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.Eventing.Reader;
+using EventLogParser;
+
+namespace EventLogParser
+{
+    class Program
+    {
+
+        static void Usage()
+        {
+            string usageString = @"
+Usage
+=====
+
+EventLogParser.exe eventid=EVENTID [outfile=C:\Windows\Temp\loggedfiles.txt]
+
+Description:
+    
+    EventLogParser will parse event IDs 4103, 4104 and 4688 to search for sensitive
+    information, including:
+        - RDP Credentials
+        - net user commands
+        - Plaintext secure-strings
+        - PSCredential objects
+        - SSH commands using keys
+        - Imported powershell modules.
+
+Arguments:
+    
+    Required:
+        
+        eventid - Must be one of:
+                    4103 - Script Block Logging
+                    4104 - PowerShell module logging
+                    4688 - Process Creation logging.
+                           Note: Must be high integrity and have
+                                 command line logging enabled.
+
+    Optional:
+   
+        outfile - Path to the file you wish to write all matching script block logs
+                  to. This only applies to event ID 4104.
+
+Example:
+
+    .\EventLogParser.exe eventid=4104 outfile=C:\Windows\Temp\scripts.txt
+
+        Writes all 4104 events with ""sensitive"" information to C:\Windows\Temp\scripts.txt
+
+    .\EventLogParser.exe eventid=4103
+        
+        List all modules path on disk that have been loaded by each user.
+";
+            Console.WriteLine(usageString);
+            Environment.Exit(1);
+        }
+
+        static Dictionary<string, string> ArgumentParser(string[] args)
+        {
+            Dictionary<string, string> results = new Dictionary<string, string>();
+            results["context"] = "3";
+            results["outfile"] = "";
+            char[] sep = { '=' };
+            foreach (string arg in args)
+            {
+                if (arg.Contains("="))
+                {
+                    string[] parts = arg.Split(sep, 2);
+                    if (parts.Length == 2)
+                    {
+                        results[parts[0].Trim().ToLower()] = parts[1].Trim();
+                    }
+                    else
+                    {
+                        Console.WriteLine("[-] Invalid argument passed. Skipping {0}.", arg);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("[-] Invalid argument passed. Skipping {0}.", arg);
+                }
+            }
+            if (!results.ContainsKey("eventid"))
+            {
+                Console.WriteLine("[X] No eventid passed.");
+                Usage();
+            }
+            if (!EventLogHelpers.supportedEventIds.ContainsKey(results["eventid"]))
+            {
+                Console.WriteLine("[X] Invalid eventid passed. You gave: {0}", results["eventid"]);
+                Usage();
+            }
+            return results;
+        }
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> arguments = ArgumentParser(args);
+            string eventid = arguments["eventid"];
+            if (eventid == "4104")
+            {
+                EventLogHelpers.supportedEventIds[eventid].DynamicInvoke(arguments["outfile"], arguments["context"]);
+            }
+            else
+            {
+                EventLogHelpers.supportedEventIds[eventid].DynamicInvoke();
+            }
+            Console.WriteLine("[*] Finished parsing {0} logs.", eventid);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,88 @@
+# EventLogParser
+
+```
+Usage
+=====
+
+EventLogParser.exe eventid=EVENTID [outfile=C:\Windows\Temp\loggedfiles.txt]
+
+Description:
+
+    EventLogParser will parse event IDs 4103, 4104 and 4688 to search for sensitive
+    information, including:
+        - RDP Credentials
+        - net user commands
+        - Plaintext secure-strings
+        - PSCredential objects
+        - SSH commands using keys
+        - Imported powershell modules.
+
+Arguments:
+
+    Required:
+
+        eventid - Must be one of:
+                    4103 - Script Block Logging
+                    4104 - PowerShell module logging
+                    4688 - Process Creation logging.
+                           Note: Must be high integrity and have
+                                 command line logging enabled.
+
+    Optional:
+
+        outfile - Path to the file you wish to write all matching script block logs
+                  to. This only applies to event ID 4104.
+
+Example:
+
+    .\EventLogParser.exe eventid=4104 outfile=C:\Windows\Temp\scripts.txt
+
+        Writes all 4104 events with ""sensitive"" information to C:\Windows\Temp\scripts.txt
+
+    .\EventLogParser.exe eventid=4103
+
+        List all modules path on disk that have been loaded by each user.
+```
+
+## Examples
+
+```
+.\EventLogParser.exe eventid=4104
+[*] Parsing PowerShell 4104 event logs...
+
+[+] Regex Match: net user $NewOsUser $NewOsPass /add & net localgroup administrators /add $NewOsUser'';"
+[+] Regex Context:
+        # Create query
+        }else{
+        Break
+        Write-Verbose "$Instance : The service account does not have local administrator privileges so no OS admin can be created.  Aborted."
+        net user $NewOsUser $NewOsPass /add & net localgroup administrators /add $NewOsUser'';"
+        # Status user
+        Write-Verbose "$Instance : Payload generated."
+        }
+        }else{
+
+[+] Regex Match: New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($Username, $secpass)
+[+] Regex Context:
+        $secpass = ConvertTo-SecureString $Password -AsPlainText -Force
+        {
+        if($Username -and $Password)
+        # Create PS Credential object
+        New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($Username, $secpass)
+        }
+        # Create Create the connection to LDAP
+        if ($DomainController)
+        {
+
+[+] Regex Match: ConvertTo-SecureString $Password -AsPlainText -Force
+[+] Regex Context:
+        {
+        if($Username -and $Password)
+        # Create PS Credential object
+        {
+        ConvertTo-SecureString $Password -AsPlainText -Force
+        $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($Username, $secpass)
+        }
+        # Create Create the connection to LDAP
+        if ($DomainController)
+```
